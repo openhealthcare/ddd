779f0f948b4dbf6c75d9cb897ea52b59d15c6a8b
David Miller <david@deadpansincerity.com>
2014-10-21 08:44:18 +0100

    Make tests use sigils

--- test/matcher_test.exs
+++ test/matcher_test.exs
@@ -15,18 +15,18 @@ defmodule MatcherTest do
     pre = %{}
     post = %{ "key" => "friday"}
 
-    {ok, res} = Ddd.Matcher.process_line(@fb, "when \"key\" is \"friday\"", {pre, post})
+    {ok, res} = Ddd.Matcher.process_line(@fb, ~s(when "key" is "friday"), {pre, post})
     assert(ok == :ok)
 
-    {ok, res} = Ddd.Matcher.process_line(@fb, "when \"key\" is \"wednesday\"", {pre, post})
+    {ok, res} = Ddd.Matcher.process_line(@fb, ~s(when "key" is "wednesday"), {pre, post})
     assert(ok == :fail)
   end
 
   test "basic match for is not" do
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" is not \"healthy\"", {@empty, @full}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" is not "healthy"), {@empty, @full}
     assert ok == :ok
 
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" is not \"CAP\"", {@empty, @full}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" is not "CAP"), {@empty, @full}
     assert ok == :fail
   end
 
@@ -34,18 +34,18 @@ defmodule MatcherTest do
     pre = %{ "key" => "friday"}
     post = %{}
 
-    {ok, res} = Ddd.Matcher.process_line(@fb, "when \"key\" was \"friday\"", {pre, post})
+    {ok, res} = Ddd.Matcher.process_line(@fb, ~s(when "key" was "friday"), {pre, post})
     assert(ok == :ok)
 
-    {ok, res} = Ddd.Matcher.process_line(@fb, "when \"key\" was \"wednesday\"", {pre, post})
+    {ok, res} = Ddd.Matcher.process_line(@fb, ~s(when "key" was "wednesday"), {pre, post})
     assert(ok == :fail)
   end
 
   test "basic match for was not" do
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" was not \"healthy\"", {@full, @empty}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" was not "healthy"), {@full, @empty}
     assert ok == :ok
 
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" was not \"CAP\"", {@full, @empty}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" was not "CAP"), {@full, @empty}
     assert ok == :fail
   end
 
@@ -53,51 +53,51 @@ defmodule MatcherTest do
     pre = %{ "key"=> "friday"}
     post = %{ "key"=> "friday"}
 
-    {ok, res} = Ddd.Matcher.process_line(@fb, "When \"key\" was \"friday\"", {pre, post})
+    {ok, res} = Ddd.Matcher.process_line(@fb, ~s(When "key" was "friday"), {pre, post})
     assert(ok == :ok)
 
-    {ok, res} = Ddd.Matcher.process_line(@fb, "When \"key\" was \"wednesday\"", {pre, post})
+    {ok, res} = Ddd.Matcher.process_line(@fb, ~s(When "key" was "wednesday"), {pre, post})
     assert(ok == :fail)
   end
 
   test "basic match for did contain" do
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" did contain \"ord\"", {@full, @empty}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" did contain "ord"), {@full, @empty}
     assert ok == :ok
 
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" did contain \"death\"", {@full, @empty}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" did contain "death"), {@full, @empty}
     assert ok == :fail
   end
 
   test "basic match for contains" do
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" contains \"ord\"", {@empty, @full}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" contains "ord"), {@empty, @full}
     assert ok == :ok
 
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" contains \"death\"", {@empty, @full}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" contains "death"), {@empty, @full}
     assert ok == :fail
   end
 
   test "basic match for did not contain" do
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" did not contain \"death\"", {@full, @empty}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" did not contain "death"), {@full, @empty}
     assert ok == :ok
 
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" did not contain \"ord\"", {@full, @empty}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" did not contain "ord"), {@full, @empty}
     assert ok == :fail
   end
 
   
   test "basic match for does not contain" do
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" does not contain \"death\"", {@empty, @full}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" does not contain "death"), {@empty, @full}
     assert ok == :ok
 
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" does not contain \"ord\"", {@empty, @full}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" does not contain "ord"), {@empty, @full}
     assert ok == :fail
   end
 
   test "basic match for changed to" do
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" changed to \"CAP\"", {@empty, @full}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" changed to "CAP"), {@empty, @full}
     assert ok == :ok
 
-    {ok, res} = Ddd.Matcher.process_line @fb, "When \"diagnosis.name\" changed to \"healthy\"", {@empty, @full}
+    {ok, res} = Ddd.Matcher.process_line @fb, ~s(When "diagnosis.name" changed to "healthy"), {@empty, @full}
     assert ok == :fail
   end
 
