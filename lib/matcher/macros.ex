ec348080ce045bbe4e0c8e0359799c295800baf4
Ross Jones <ross@servercode.co.uk>
2014-10-28 19:02:52 +0000

    More macro magic to create a list to match a line on

--- lib/matcher/macros.ex
+++ lib/matcher/macros.ex
@@ -1,12 +1,32 @@
 defmodule Ddd.Macros  do
 
+defmacro __using__(_) do
+    quote do
+      import unquote(__MODULE__)
+      @before_compile unquote(__MODULE__)
+      @rules = []
+    end
+  end
 
-    defmacro defrule(signature, body) do
-      quote do
-        def unquote(signature) do
-          unquote(body[:do])
-        end
+  defmacro defrule(signature, body) do
+    f = elem(signature, 0)
+    vl = elem(signature, 2)
+
+    m = Enum.map hd(tl(vl)), fn (x) ->
+      case x do
+        {_, _, _} -> ""
+        _ -> x
       end
     end
+
+    # We want to add this to @rules, but I am totally failing
+    # IO.inspect [f|m]
+
+    quote do
+      def unquote(signature) do
+        unquote(body[:do])
+      end
+    end
+  end
 
 end
